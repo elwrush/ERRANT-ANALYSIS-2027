f6f34f3f2dd1bda9bf6c465a9544c79fe1f83594
elwrush <148333209+elwrush@users.noreply.github.com>
2026-09-12 06:47:04 +0000

    backup: 2026-09-12

--- backups/data.sql
+++ backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict cvjFElXyLplfErVm4gJDMhMBmG23H6IECNuTt0eis4qkwpQJLGCFxAB0lO8Ytgn
+-- \restrict D3oFklEE5PCdgFW3bgGXHFNpiip3elF3ISBx9byEd9LQLK56I9kDxsdT8QfbUzz
 
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.6
@@ -16095,6 +16095,6 @@ SELECT pg_catalog.setval('"public"."student_submissions_id_seq"', 4145, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict cvjFElXyLplfErVm4gJDMhMBmG23H6IECNuTt0eis4qkwpQJLGCFxAB0lO8Ytgn
+-- \unrestrict D3oFklEE5PCdgFW3bgGXHFNpiip3elF3ISBx9byEd9LQLK56I9kDxsdT8QfbUzz
 
 RESET ALL;
